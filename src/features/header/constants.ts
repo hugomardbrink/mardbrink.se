4b48c852c68854f4a5e1cf46356cac6ce598a6ad
hugomardbrink <hugo@mardbrink.se>
2022-07-19 00:51:44 +0200

    Implement header with escape hatch and social media icons

---
+++ src/features/header/constants.ts
@@ -0,0 +1,2 @@
+export const GITHUB_URL = 'https://github.com/hugomardbrink'
+export const LINKEDIN_URL = 'https://linkedin.com/in/hugomardbrink'
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Header } from '../features/header/Header'
 import { Wrapper } from './App.styled'
 
 const App = () => {
-  return <Wrapper>Home</Wrapper>
+  return (
+    <Wrapper>
+      <Header />
+    </Wrapper>
+  )
 }
 
 export default App
