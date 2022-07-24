bfac60b4f7d410308e181b6178473310c8c5451a
hugomardbrink <hugo@mardbrink.se>
2022-07-25 00:23:13 +0200

    Change to new colors

--- src/pages/Main.styled.ts
+++ src/pages/Main.styled.ts
@@ -7,14 +7,14 @@ export const Wrapper = styled.main`
   left: 0;
   top: 0;
   overflow: hidden;
-  background-color: ${({ theme }) => theme.colors.sky};
+  background-color: ${({ theme }) => theme.colors.background};
   z-index: -1000;
 `
 
 export const Content = styled.div`
   box-sizing: border-box;
   border: solid ${({ theme }) => theme.colors.text} 10px;
-  background-color: ${({ theme }) => theme.colors.ocean.shallow};
+  background-color: ${({ theme }) => theme.colors.background};
   margin: 50px 100px;
   overflow-y: scroll;
   height: calc(100% - 100px);
