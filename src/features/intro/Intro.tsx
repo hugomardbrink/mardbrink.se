fa7a36a7858cf65d6f4668e45635baf63b6d67df
hugomardbrink <hugo@mardbrink.se>
2022-07-25 21:38:44 +0200

    Add about me text

--- src/features/intro/Intro.tsx
+++ src/features/intro/Intro.tsx
@@ -1,9 +1,17 @@
-import { Text, Wrapper } from './Intro.styled'
+import { AboutContainer, AboutHeader, AboutText, IntroText, Wrapper } from './Intro.styled'
 
 export const Intro = () => {
   return (
     <Wrapper>
-      <Text>Computer science student with a passion for learning.</Text>
+      <IntroText>Computer science student with a passion for learning.</IntroText>
+      <AboutContainer>
+        <AboutHeader>About me!</AboutHeader>
+        <AboutText>
+          Hello! I'm a 21-year-old full time student, based in Gothenburg. I have a great interest
+          in exploring and experimenting with technologies, usually through hobby projects. Besides
+          studying full time I've also worked at Opera and Volvo.
+        </AboutText>
+      </AboutContainer>
     </Wrapper>
   )
 }
