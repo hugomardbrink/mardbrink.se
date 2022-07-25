d627ba44aadbcf929872dc933f9ae62c9350e815
hugomardbrink <hugo@mardbrink.se>
2022-07-25 21:38:44 +0200

    Add about me text

--- src/features/intro/Intro.styled.ts
+++ src/features/intro/Intro.styled.ts
@@ -1,15 +1,35 @@
 import styled from 'styled-components/macro'
 
 export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
   height: auto;
-  margin-left: 36px;
-  margin-top: 100px;
-  width: 50%;
+  margin: 100px 32px;
 `
 
-export const Text = styled.div`
+export const IntroText = styled.div`
   font-size: 64px;
   font-weight: 800;
   padding-left: 16px;
+  margin-left: 4px;
   border-left: solid ${({ theme }) => theme.colors.text} 10px;
+  width: 50%;
+`
+
+export const AboutContainer = styled.div`
+  justify-self: right;
+  align-self: flex-end;
+  margin-top: 130px;
+  text-align: right;
+  width: 43%;
+`
+
+export const AboutHeader = styled.div`
+  font-size: 28px;
+  font-weight: 600;
+`
+
+export const AboutText = styled.div`
+  font-size: 20px;
+  margin-top: 6px;
 `
