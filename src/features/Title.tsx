49702232f37a6a9700cc3e9c9d8fe09c6c0af9e6
Hugo Mårdbrink <hugo@mardbrink.se>
2022-08-13 00:23:59 +0200

    Adapt theme to new design

---
+++ src/features/Title.tsx
@@ -0,0 +1,10 @@
+import { NameContainer, Wrapper } from './Title.styled'
+
+export const NameTitle = () => {
+  return (
+    <Wrapper>
+      <NameContainer>Hugo</NameContainer>
+      <NameContainer>Mardbrink</NameContainer>
+    </Wrapper>
+  )
+}
