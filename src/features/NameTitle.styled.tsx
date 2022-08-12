ed6e5ba5fd821b46c26112ffc9879092bc963f7d
Hugo Mårdbrink <hugo@mardbrink.se>
2022-08-13 00:14:01 +0200

    Add Ailerons font

---
+++ src/features/NameTitle.styled.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components/macro'
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 10vw;
+`
+
+export const NameContainer = styled.div`
+  font-family: Ailerons;
+  font-size: 160px;
+`
