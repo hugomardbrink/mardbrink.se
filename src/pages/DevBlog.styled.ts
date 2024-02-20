b315f1a4640534339bcfab13caa18d9e0ee5ea86
Hugo Mårdbrink <hugo@mardbrink.se>
2024-02-20 13:49:14 +0100

    Add post modules

--- src/pages/DevBlog.styled.ts
+++ src/pages/DevBlog.styled.ts
@@ -28,3 +28,56 @@ export const HomeLink = styled.a`
     text-decoration: underline;
   }
 `
+
+export const PostModule = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 70px;
+  width: 550px;
+  height: 250px;
+  border: 5px ${({ theme }) => theme.colors.secondary} solid;
+  box-shadow: 9px 9px 0 1px ${({ theme }) => theme.colors.secondary};
+`
+
+export const PostTitle = styled.span`
+  margin-top: 30px;
+  margin-left: 35px;
+
+  font-size: 36px;
+  font-weight: 600;
+`
+
+export const PostDate = styled.span`
+  margin-top: 7px;
+  margin-left: 37px;
+
+  font-size: 20px;
+  font-weight: 500;
+`
+
+export const ModulePrimaryButton = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+
+  width: 200px;
+  height: 60px;
+  background-color: ${({ theme }) => theme.colors.secondary};
+  color: ${({ theme }) => theme.colors.primary};
+  font-size: 20px;
+  border: none;
+  cursor: pointer;
+
+  transition: box-shadow 0.25s, margin 0.25s;
+
+  box-shadow: 0 0 0 0 ${({ theme }) => theme.colors.accent};
+  margin-top: 75px;
+  margin-left: 35px;
+
+  &:hover {
+    margin-top: 66px;
+    margin-left: 26px;
+    box-shadow: 9px 9px 0 1px ${({ theme }) => theme.colors.accent};
+  }
+`
--- src/pages/DevBlog.tsx
+++ src/pages/DevBlog.tsx
@@ -1,10 +1,29 @@
-import { Title, HomeLink, Wrapper } from './DevBlog.styled'
+import { Title, HomeLink, PostModule, PostTitle, PostDate, ModulePrimaryButton, Wrapper } from './DevBlog.styled'
+import testPost from '../../res/posts/Test.md?raw'
 
 export const DevBlog = () => {
+  interface Post {
+    name: string
+    date: Date
+    text: string
+  }
+
+  const Posts: Post[] = [{ name: 'TEST POST', date: new Date(2024, 1, 20, 13, 20), text: testPost }]
+
   return (
     <Wrapper>
       <HomeLink href="/">&lt; HOME</HomeLink>
       <Title>DEV BLOG</Title>
+      {Posts.map((p) => (
+        <PostModule>
+          <PostTitle>{p.name}</PostTitle>
+          <PostDate>
+            {p.date.getFullYear()}-{p.date.getMonth()}-{p.date.getDay()} @{p.date.getHours()}:
+            {p.date.getMinutes()}
+          </PostDate>
+          <ModulePrimaryButton>READ POST</ModulePrimaryButton>
+        </PostModule>
+      ))}
     </Wrapper>
   )
 }
